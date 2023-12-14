b2cf7ad6faf48267f561edaaf2a192cee61b6037
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-14 20:38:04 +0700

    cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
-import java.io.IOException;
-import java.util.Date;
-import java.util.Scanner;
-import java.util.concurrent.CountDownLatch;
 
-import Data.Enums.Enums;
 import Statics.DI;
 import Statics.DI.*;
-import Util.Formatting;
-import Util.Generator;
-import View.MainMenu.MainMenu;
 
-import Util.Encryption;
-import ViewModel.AuthViewModel.AuthViewModel;
-
-import static Statics.DI.authViewModel;
-//import View.MainMenu.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         DI.init();
 
     }
