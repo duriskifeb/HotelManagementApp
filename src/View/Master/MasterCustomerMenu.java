e7a4a333bdfe89a9af9033e94307680a67b10147
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-14 18:34:33 +0700

    adde new task to David

---
+++ src/View/Master/MasterCustomerMenu.java
@@ -0,0 +1,7 @@
+package View.Master;
+
+public class MasterCustomerMenu {
+
+     // TODO @David
+    // add new menu methods to Create (Register), View, Update, and Delete Customer
+}
---
+++ src/View/Master/MasterKamarMenu.java
@@ -0,0 +1,6 @@
+package View.Master;
+
+public class MasterKamarMenu {
+    // TODO @David
+    // add new sub menu methods to Create (Register), View, Update, and Delete Kamar
+}
