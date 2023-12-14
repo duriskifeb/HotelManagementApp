ae9b6c9fbac487ba898da19c91e007d682128363
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-14 07:05:45 +0700

    added enums

--- src/DataSource/Enums/Enums.java
+++ src/DataSource/Enums/Enums.java
@@ -2,4 +2,26 @@ package DataSource.Enums;
 
 public class Enums {
     // put enums here
+    enum StatusKamar {
+        AVAILABLE,
+        OCCUPIED,
+        BOOKED,
+        CLEANING
+    }
+
+    enum UserRole {
+        PEGAWAI,
+        MANAGER
+    }
+
+    enum Pembayaran{
+        BANK,
+        CASH
+    }
+
+    enum StatusTransaksi{
+        PENDING,
+        ONGOING,
+        DONE
+    }
 }
