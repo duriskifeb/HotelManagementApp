7f19e17464977995290d8c20ab033cee1685b2e5
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-14 07:48:41 +0700

    added Encryption

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
-import View.MainMenu.*;
+
+import Util.Encryption;
+//import View.MainMenu.*;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        MainMenu menu = new MainMenu();
 
-        menu.login();
+        String ayamHashed = Encryption.hashPassword("ayam");
+//        MainMenu menu = new MainMenu();
+
+//        menu.login();
     }
 } 
---
+++ src/Util/Encryption.java
@@ -0,0 +1,28 @@
+package Util;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class Encryption {
+    public static String hashPassword(String password){
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        Base64.Encoder enc = Base64.getEncoder();
+
+//        System.out.printf("hash: %s%n", enc.encodeToString(hash));
+
+        return enc.encodeToString(hash);
+    }
+}
