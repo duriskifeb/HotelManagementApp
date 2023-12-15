887f040fb7dc121329cfe527692a2cbd4257d9b2
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-15 13:22:59 +0700

    implementing master pegawai viewmodel

--- src/ViewModel/MasterViewModel/MasterPegawaiViewModel.java
+++ src/ViewModel/MasterViewModel/MasterPegawaiViewModel.java
@@ -1,7 +1,11 @@
 package ViewModel.MasterViewModel;
 
+import Data.Enums.Enums;
+import Data.Model.Kamar;
 import Data.Model.User;
 import Domain.Master.MasterPegawai;
+import Util.Encryption;
+import Util.Formatting;
 
 import static Util.Formatting.formatMessageOutput;
 
@@ -21,5 +25,56 @@ public class MasterPegawaiViewModel {
         }
     }
 
+    public void addNewPegawai(
+            String nama,
+            String email,
+            String password,
+            Enums.UserRole role
+    ){
+        User newData = new User(email, nama,  role, Encryption.hashPassword(password));
+        masterPegawai.addNewPegawai(newData);
+    }
+
+    public void deletePegawai(String userID) {
+        masterPegawai.deletePegawai(userID);
+    }
+    public void editDataPegawai(
+            String nama,
+            String email,
+            Enums.UserRole role
+    ){
+        User newData = new User(email, nama,  role, selectedPegawai.getPassword());
+
+        if(selectedPegawai != null){
+            masterPegawai.editDataPegawai(selectedPegawai, newData);
+        }
+    }
+
+    public void viewAllDataPegawai(){
+        masterPegawai.getAllPegawai()
+                .stream()
+                .iterator()
+                .forEachRemaining(
+                        user -> {
+                            String idPegawai = user.getUserID();
+                            String nama = user.getNama();
+                            String email = user.getEmail();
+                            Enums.UserRole role = user.getRole();
+                            System.out.format("%10s %10s \t%15s \t%10s\n", idPegawai, nama, email, role);
+
+                        }
+                );
+
+    }
+
+    public void viewSelectedPegawai(){
+        if(selectedPegawai != null){
+            String idPegawai = selectedPegawai.getUserID();
+            String nama = selectedPegawai.getNama();
+            String email = selectedPegawai.getEmail();
+            Enums.UserRole role = selectedPegawai.getRole();
+            System.out.format("%10s %10s \t%15s \t%10s\n", idPegawai, nama, email, role);
+        }
+    }
 
 }
