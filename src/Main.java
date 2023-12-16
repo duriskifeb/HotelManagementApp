4b3e05c04c2d31fec109399f553965e58b04535b
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-16 18:42:01 +0700

    finish method view transaksi

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 
+import Data.Enums.Enums;
 import Statics.DI;
 import Statics.DI.*;
 import Statics.StaticsInput;
 import Util.Formatting;
+
+import java.util.Date;
 
 import static Statics.DI.authViewModel;
 import static Statics.DI.masterCustomerVM;
@@ -13,13 +16,8 @@ public class Main {
         DI.init();
         StaticsInput.cls();
 
-        authViewModel.doLogin("ADM01", "ayamgoyeng");
-        try{
-            System.out.println(authViewModel.loggedUser.getNama());
-        } catch (Exception e){
 
-        }
 
-//        masterCustomerVM.viewAllDataCustomer();
+
     }
 } 
--- src/ViewModel/MasterViewModel/MasterTransaksiViewModel.java
+++ src/ViewModel/MasterViewModel/MasterTransaksiViewModel.java
@@ -24,45 +24,51 @@ public class MasterTransaksiViewModel {
     public Transaksi selectedTransaksi;
 
     // EDITS REGION
-    public void addKamar(String noKamar){
+    public void addKamar(String noKamar) {
         transaksiUseCase.addKamar(noKamar);
         commitTransaksi();
         updateStateSelectedTransaksi();
     }
-    public void removeKamar(String noKamar){
+
+    public void removeKamar(String noKamar) {
         transaksiUseCase.removeKamar(noKamar);
         commitTransaksi();
         updateStateSelectedTransaksi();
     }
+
     public void addCustomer(String NIK) {
         transaksiUseCase.addCustomer(NIK);
         commitTransaksi();
         updateStateSelectedTransaksi();
     }
-    public void removeCustomer(String NIK){
+
+    public void removeCustomer(String NIK) {
         transaksiUseCase.removeCustomer(NIK);
         commitTransaksi();
         updateStateSelectedTransaksi();
     }
+
     public void checkOut() {
         transaksiUseCase.checkOut();
         updateStateSelectedTransaksi();
         commitTransaksi();
     }
+
     public void checkIn() {
         transaksiUseCase.checkIn();
         commitTransaksi();
     }
+
     public void bayar(Enums.Pembayaran metodeBayar, double amountBayar) {
         transaksiUseCase.bayar(metodeBayar, amountBayar);
         commitTransaksi();
     }
     // EDITS REGION
 
-    public void commitTransaksi(){
-        if(this.selectedTransaksi != null){
+    public void commitTransaksi() {
+        if (this.selectedTransaksi != null) {
             transaksiUseCase.commitTransaksi();
-        }else{
+        } else {
             Formatting.formatMessageOutput("Tidak ada transaksi yang dipilih / data sudah tidak ada");
         }
     }
@@ -77,15 +83,15 @@ public class MasterTransaksiViewModel {
         }
     }
 
-    private void updateStateSelectedTransaksi(){
+    private void updateStateSelectedTransaksi() {
         this.selectedTransaksi = transaksiUseCase.getCurrentActiveTransaksi();
     }
 
     public void deleteTransaksi() {
-        if(selectedTransaksi != null) {
+        if (selectedTransaksi != null) {
             masterTransaksi.deleteTransaksi(this.selectedTransaksi.getNoTransaksi());
-            this.selectedTransaksi= null;
-        }else{
+            this.selectedTransaksi = null;
+        } else {
             Formatting.formatMessageOutput("Pilih transaksi yang akan di delete");
         }
 
@@ -97,13 +103,11 @@ public class MasterTransaksiViewModel {
                 .iterator()
                 .forEachRemaining(
                         selectedTransaksi -> {
-
-                            // TODO @David show preview data transaksi
-//                            String idPegawai = user.getUserID();
-//                            String nama = user.getNama();
-//                            String email = user.getEmail();
-//                            Enums.UserRole role = user.getRole();
-//                            System.out.format("%10s %10s \t%15s \t%10s\n", idPegawai, nama, email, role);
+                            String noTransaksi = selectedTransaksi.getNoTransaksi();
+                            String user = selectedTransaksi.getCustomers().get(0).getNama();
+                            Enums.StatusTransaksi status = selectedTransaksi.getStatusTransaksi();
+                            String tanggalTransaksi = Formatting.formatDate(selectedTransaksi.getTanggalTransaksi());
+                            System.out.format("%20s %25s \t%20s \t%20s\n", noTransaksi, user, status, tanggalTransaksi);
 
                         }
                 );
@@ -111,8 +115,55 @@ public class MasterTransaksiViewModel {
 
     public void viewDetailSelectedTransaksi() {
         if (selectedTransaksi != null) {
-            // TODO @David show all data in transaksi (detail)
+            String noTransaksi = this.selectedTransaksi.getNoTransaksi();
+            String user = this.selectedTransaksi.getPegawai().getUserID();
+            ;
+            Enums.StatusTransaksi status = this.selectedTransaksi.getStatusTransaksi();
+            String tanggalTransaksi = Formatting.formatDate(this.selectedTransaksi.getTanggalTransaksi());
+
+            System.out.println("========================================= DETAIL TRANSAKSI =========================================");
+            System.out.println("No Transaksi: " + noTransaksi);
+            System.out.println("PIC : " + "Nama PIC" + "( " + "PIC ID AYAM" + " )");
+            System.out.println("---------------------------------------------------------------------------------------------------");
+            System.out.println("Pelanggan : " + user);
+            System.out.println("Status: " + status);
+            System.out.println("Tanggal : " + tanggalTransaksi);
+            System.out.println("---------------------------------------------------------------------------------------------------");
+            System.out.println("Start : " + Formatting.formatDate(new Date()));
+            System.out.println("End : " + Formatting.formatDate(new Date()));
+            System.out.println("---------------------------------------------------------------------------------------------------");
+            System.out.println("Check In : " + Formatting.formatDate(new Date()));
+            System.out.println("Check Out : " + Formatting.formatDate(new Date()));
+
+            System.out.println("========================================= DETAIL PESANAN =========================================");
+            System.out.println("KAMAR : ");
+
+            this.selectedTransaksi.getKamarOrdered().stream().iterator().forEachRemaining(
+                    kamar -> {
+                        System.out.format("%20s %25s \t%20s \t%20s\n", kamar.getNoKamar(), kamar.getJenisKamar(), kamar.getHarga(), kamar.getKapasitas());
+                    }
+            );
+
         }
+
+        System.out.println();
+
+        System.out.println("CUSTOMER : ");
+        this.selectedTransaksi.getCustomers().stream().iterator().forEachRemaining(
+                customer -> {
+                    System.out.format("%20s %25s \t%20s \t%20s\n", customer.getNIK(), customer.getNama(), customer.getTelp(), customer.getEmail());
+                }
+        );
+
+        System.out.println("========================================= DETAIL BIAYA =========================================");
+        System.out.println("TOTAL : " + Formatting.formatRupiah(this.selectedTransaksi.getTotal()));
+        System.out.println("DIBAYARKAN : " + Formatting.formatRupiah(this.selectedTransaksi.getPaid()));
+        System.out.println("Status Pembayaran : " + this.selectedTransaksi.getStatusPembayaran() + "( " + this.selectedTransaksi.getPembayaran() + " )");
+        System.out.println("========================================= DETAIL TRANSAKSI =========================================");
+
+
     }
 
 }
+
+
