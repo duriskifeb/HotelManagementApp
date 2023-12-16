d229c518394559f291f59401f98c3dc60ef74873
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-16 18:50:21 +0700

    move method view transaksi

--- src/ViewModel/MasterViewModel/MasterTransaksiViewModel.java
+++ src/ViewModel/MasterViewModel/MasterTransaksiViewModel.java
@@ -97,72 +97,7 @@ public class MasterTransaksiViewModel {
 
     }
 
-    public void viewAllTransaksi() {
-        masterTransaksi.getAllTransaksi()
-                .stream()
-                .iterator()
-                .forEachRemaining(
-                        selectedTransaksi -> {
-                            String noTransaksi = selectedTransaksi.getNoTransaksi();
-                            String user = selectedTransaksi.getCustomers().get(0).getNama();
-                            Enums.StatusTransaksi status = selectedTransaksi.getStatusTransaksi();
-                            String tanggalTransaksi = Formatting.formatDate(selectedTransaksi.getTanggalTransaksi());
-                            System.out.format("%20s %25s \t%20s \t%20s\n", noTransaksi, user, status, tanggalTransaksi);
 
-                        }
-                );
-    }
-
-    public void viewDetailSelectedTransaksi() {
-        if (selectedTransaksi != null) {
-            String noTransaksi = this.selectedTransaksi.getNoTransaksi();
-            String user = this.selectedTransaksi.getPegawai().getUserID();
-            ;
-            Enums.StatusTransaksi status = this.selectedTransaksi.getStatusTransaksi();
-            String tanggalTransaksi = Formatting.formatDate(this.selectedTransaksi.getTanggalTransaksi());
-
-            System.out.println("========================================= DETAIL TRANSAKSI =========================================");
-            System.out.println("No Transaksi: " + noTransaksi);
-            System.out.println("PIC : " + "Nama PIC" + "( " + "PIC ID AYAM" + " )");
-            System.out.println("---------------------------------------------------------------------------------------------------");
-            System.out.println("Pelanggan : " + user);
-            System.out.println("Status: " + status);
-            System.out.println("Tanggal : " + tanggalTransaksi);
-            System.out.println("---------------------------------------------------------------------------------------------------");
-            System.out.println("Start : " + Formatting.formatDate(new Date()));
-            System.out.println("End : " + Formatting.formatDate(new Date()));
-            System.out.println("---------------------------------------------------------------------------------------------------");
-            System.out.println("Check In : " + Formatting.formatDate(new Date()));
-            System.out.println("Check Out : " + Formatting.formatDate(new Date()));
-
-            System.out.println("========================================= DETAIL PESANAN =========================================");
-            System.out.println("KAMAR : ");
-
-            this.selectedTransaksi.getKamarOrdered().stream().iterator().forEachRemaining(
-                    kamar -> {
-                        System.out.format("%20s %25s \t%20s \t%20s\n", kamar.getNoKamar(), kamar.getJenisKamar(), kamar.getHarga(), kamar.getKapasitas());
-                    }
-            );
-
-        }
-
-        System.out.println();
-
-        System.out.println("CUSTOMER : ");
-        this.selectedTransaksi.getCustomers().stream().iterator().forEachRemaining(
-                customer -> {
-                    System.out.format("%20s %25s \t%20s \t%20s\n", customer.getNIK(), customer.getNama(), customer.getTelp(), customer.getEmail());
-                }
-        );
-
-        System.out.println("========================================= DETAIL BIAYA =========================================");
-        System.out.println("TOTAL : " + Formatting.formatRupiah(this.selectedTransaksi.getTotal()));
-        System.out.println("DIBAYARKAN : " + Formatting.formatRupiah(this.selectedTransaksi.getPaid()));
-        System.out.println("Status Pembayaran : " + this.selectedTransaksi.getStatusPembayaran() + "( " + this.selectedTransaksi.getPembayaran() + " )");
-        System.out.println("========================================= DETAIL TRANSAKSI =========================================");
-
-
-    }
 
 }
 
