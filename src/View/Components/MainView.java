dd705fe75d1f5642dd23511f1d46411264c89cb6
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-16 20:42:29 +0700

    move app hedaer

---
+++ src/View/Components/MainView.java
@@ -0,0 +1,11 @@
+package View.Components;
+
+public class MainView {
+
+    public static void appHeader() {
+        System.out.println("==============================");
+        System.out.println(" HOTEL MANAGEMENT APLICATION ");
+        System.out.println("==============================");
+    }
+
+}
