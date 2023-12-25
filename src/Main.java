2a4302f60946b9152fa46a40432d39169657e05e
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-12-25 19:01:26 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -10,40 +10,40 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args)  {
         DI.init();
-//        HotelManagementApp app = new HotelManagementApp();
-//        app.run();
+        HotelManagementApp app = new HotelManagementApp();
+        app.run();
 
-        LinkedList<AppRouter.AppRoute> routeList = new LinkedList();
-
-        routeList.add(AppRouter.AppRoute.MAIN_MENU);
-
-        System.out.println(routeList);
-        System.out.println("navigate");
-        System.out.println();
-
-        routeList.add(AppRouter.AppRoute.MASTER_CUSTOMER);
-
-        System.out.println(routeList);
-        System.out.println("navigate");
-        System.out.println();
-
-        routeList.add(AppRouter.AppRoute.SUB_MASTER_DETAIL_CUSTOMER);
-
-        System.out.println(routeList);
-        System.out.println("navigate");
-        System.out.println();
-
-        System.out.println("current");
-        System.out.println(routeList.getLast());
-        System.out.println();
-
-        System.out.println("back");
-        routeList.removeLast();
-        System.out.println(routeList);
-
-        System.out.println("current");
-        System.out.println(routeList.getLast());
-        System.out.println();
+//        LinkedList<AppRouter.AppRoute> routeList = new LinkedList();
+//
+//        routeList.add(AppRouter.AppRoute.MAIN_MENU);
+//
+//        System.out.println(routeList);
+//        System.out.println("navigate");
+//        System.out.println();
+//
+//        routeList.add(AppRouter.AppRoute.MASTER_CUSTOMER);
+//
+//        System.out.println(routeList);
+//        System.out.println("navigate");
+//        System.out.println();
+//
+//        routeList.add(AppRouter.AppRoute.SUB_MASTER_DETAIL_CUSTOMER);
+//
+//        System.out.println(routeList);
+//        System.out.println("navigate");
+//        System.out.println();
+//
+//        System.out.println("current");
+//        System.out.println(routeList.getLast());
+//        System.out.println();
+//
+//        System.out.println("back");
+//        routeList.removeLast();
+//        System.out.println(routeList);
+//
+//        System.out.println("current");
+//        System.out.println(routeList.getLast());
+//        System.out.println();
 
     }
 } 
